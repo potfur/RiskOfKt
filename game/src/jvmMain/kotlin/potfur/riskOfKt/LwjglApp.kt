260d147ce6ff6ccba2891144ad688a0779530b79
Michal Wachowski <wachowski.michal@gmail.com>
2024-06-19 19:58:41 +0200

    LWJGL uses canvas size from Game instead own

--- game/src/jvmMain/kotlin/potfur/riskOfKt/LwjglApp.kt
+++ game/src/jvmMain/kotlin/potfur/riskOfKt/LwjglApp.kt
@@ -5,8 +5,6 @@ import com.lehaine.littlekt.graphics.Color
 
 fun main() {
     createLittleKtApp {
-        width = 500
-        height = 1200
         backgroundColor = Color.DARK_GRAY
         title = "Risk of Kt"
     }.start {
